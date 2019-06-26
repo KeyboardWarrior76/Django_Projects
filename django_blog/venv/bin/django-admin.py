01186421d209c5a73813f3654a282bc857641b9c
3mpirical <3mpirical10510@gmail.com>
2019-06-26 12:47:41 -0600

    deleted django blog example project

--- django_blog/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/3mpirical/Development/Programs/Django/django_blog/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
