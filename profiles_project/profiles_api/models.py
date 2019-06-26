50e0b20fffb1e2422005159f4e013908119314e8
3mpirical <3mpirical10510@gmail.com>
2019-06-26 13:48:10 -0600

    created custom user model, and configured settings to use it

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(PermissionsMixin):
+    """manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """create a new user profile"""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+        def create_super_user(self, email, name, password):
+            """create new superuser"""
+            user = self.create_user(email, name, password)
+            user.is_superuser = True;
+            user.is_staff = True;
+            user.save(using=self.db)
+
+            return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """database model for users in the system"""
@@ -10,9 +37,19 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     # ^^^ this allows us to 'deactivate' accounts from use in the future (if desired)
     is_staff = models.BooleanField(default=False)
 
+    objects = UserProfileManager()
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
+    def get_full_name(self):
+        """retrieve user's full name"""
+        return self.name
+
+    def get_short_name(self):
+        """retrieves user's short name"""
+        return self.name
 
     def ___str___(self):
         """defines the text displayed on model instances"""
-        return ""
+        return self.email
