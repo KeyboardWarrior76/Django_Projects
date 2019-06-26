86218593113d0be6510bcb51ff5f2353b6967e24
3mpirical <3mpirical10510@gmail.com>
2019-06-26 13:21:43 -0600

    ran server for first testing

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """database model for users in the system"""
+    email = model.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    # ^^^ this allows us to 'deactivate' accounts from use in the future (if desired)
+    is_staff = models.BooleanField(default=False)
+
+
+
+
+    def ___str___(self):
+        """defines the text displayed on model instances"""
+        return ""
