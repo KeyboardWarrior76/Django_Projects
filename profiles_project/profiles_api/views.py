5c32df1c86d1718e61afa3687adf15080e5f84cf
3mpirical <3mpirical10510@gmail.com>
2019-06-27 16:40:59 -0600

    added UserProfileSerializer and UserProfileModelViewSet

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 
 from profiles_api import serializers
+from profiles_api import models
 
 
 class HelloApiView(APIView):
@@ -43,3 +45,52 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """deletes an object"""
         return Response({ "method": "DELETE" })
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """test api viewset"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        return Response({
+            "message": "hello world",
+            "data": [
+                "Uses actions(list, create, retrieve, update, partial_update, destroy)",
+                "Automatically maps to URLs using Routers",
+                "Provides more functionality with less code",
+            ]
+        })
+
+    def create(self, request):
+        """create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name}, you're a cool dude, bro"
+            return Response({ "message": message })
+        else:
+            return Response( serializer.errors, status=400 )
+
+    def retrieve(self, request, pk=None):
+        """handle getting an object by its ID"""
+        return Response({ "http_method": "GET (by ID)" })
+
+    def update(self, request, pk=None):
+        """update an entire object"""
+        return Response({ "http_method": "PUT" })
+
+    def partial_update(self, request, pk=None):
+        """update only specified parameters on object"""
+        return Response({ "http_method": "PATCH" })
+
+    def destoy(self, request, pk=None):
+        """delete an object"""
+        return Response({ "http_method": "DELETE" })
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """handle crud actions for user profiles"""
+    serializer_class = serializers.USerProfileSerializer    
+    queryset = models.UserProfile.objects.all()
